d2439305c6b958b3e455b6e6de78b329a9af2573
skarya22 <51128536+skarya22@users.noreply.github.com>
2021-01-10 21:23:10 -0500

    Add files via upload

--- info_box.js
+++ info_box.js
@@ -1,5 +1,5 @@
 // Make the info box slide in
-$('h1').hover(function() {
+$('#i_button').hover(function() {
     $('#info_box').css('display','inline');
     $('#info_box').animate({left: '30%'})
 },)
